a47497168c53170c03a7f1e341390e41879a44fe
Mattias <mattias.fhc@gmail.com>
2024-12-06 21:37:51 +0100

    Day 6

--- day6/main.py
+++ day6/main.py
@@ -1,8 +1,9 @@
+# Not pretty and not optimized code, sorry :)
 
-
+# Common start
 
 grid = []
-guard = None
+startPosition = None
 obstacles = set()
 
 with open("input.txt") as file:
@@ -10,13 +11,62 @@ with open("input.txt") as file:
         grid.append([])
         for ch in line.strip():
             if ch == '#':
-                obstacles.add((len(grid) - 1, line.index(ch)))
+                obstacles.add((len(grid) - 1, len(grid[-1])))
                 None
             elif ch == '^':
-                guard = (len(grid) - 1, line.index(ch))
+                startPosition = (len(grid) - 1, len(grid[-1]))
             grid[-1].append(ch)
 
-print(grid)
-print(guard)
-print(obstacles)
+nextDirection = {(-1, 0):(0, 1), (0, 1):(1, 0), (1, 0):(0, -1), (0, -1):(-1, 0)}
 
+position = startPosition
+direction = (-1, 0)
+distinctPositions = set()
+
+def OutOfBounds(position):
+    r, c = position
+    return r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0])
+
+def walk(position, direction):
+    if position not in distinctPositions:
+        distinctPositions.add(position)
+    r, c = position
+    dr, dc = direction
+    nr, nc = r + dr, c + dc
+    if (nr, nc) in obstacles:
+        return (r, c), nextDirection[direction]
+    return (nr, nc), direction
+
+while not OutOfBounds(position):
+    position, direction = walk(position, direction)
+
+print(len(distinctPositions)) # Answer for part 1
+
+# Part 2
+
+distinctPositions.remove(startPosition)
+
+def walk2(position, direction, distinctPositionsTmp):
+    distinctPositionsTmp.add((position, direction))
+    r, c = position
+    dr, dc = direction
+    nr, nc = r + dr, c + dc
+    if (nr, nc) in obstacles:
+        return (r, c), nextDirection[direction]
+    return (nr, nc), direction
+
+def isLoop(position, direction):
+    distinctPositionsTmp = set()
+    while not OutOfBounds(position):
+        if (position, direction) in distinctPositionsTmp:
+            return 1
+        position, direction = walk2(position, direction, distinctPositionsTmp)
+    return 0
+
+result = 0
+for pos in distinctPositions:
+    obstacles.add(pos)
+    result += isLoop(startPosition, (-1, 0))
+    obstacles.remove(pos)
+
+print(result) # Answer for part 2
